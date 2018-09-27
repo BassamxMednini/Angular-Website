80a5a88fb1dbd5c8c4a510509d2af02fd977c125
Bassam Mednini <Bassam.Mednini.1997@gmail.com>
2018-09-28 00:23:08 +0200

    Router importiert

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Observable } from 'rxjs';
+import { ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-users',
