cd842d36af58918fba940c50b1caab24715a91c6
Bassam Mednini <Bassam.Mednini.1997@gmail.com>
2018-09-27 02:07:31 +0200

    users component styles in css umgeändert

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.css']
 })
 
 export class UsersComponent implements OnInit {
